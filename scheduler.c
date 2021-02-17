d180c71a72049be187aa92029b8601aa1217075a
edarsh7 <edarsh7@gmail.com>
2021-02-16 21:48:39 -0800

    idk

--- scheduler.c
+++ scheduler.c
@@ -7,17 +7,44 @@
 #include "simulator.h"
 #include "scheduler.h"
 
+
+
+/*
+ code for list implementation borrowed from https://www.programmingsimplified.com/c/data-structures/c-program-implement-linked-list
+*/
+//=-----------------------------=
+typedef struct node {
+    thread_t thread;
+    struct node * next;
+} node;
+
+
+struct node *head = NULL;
+void insert_at_end(int);
+void delete_from_begin();
+int count = 0;
+//=-----------------------------=
+
+
+
+
 void scheduler(enum algorithm algorithm, unsigned int quantum) { }
 
 void sim_tick() { }
 
-void sys_exec(thread_t *t) { }
+void sys_exec(thread_t *t) 
+{ 
+  insert_at_end(t);
+}
 
 void sys_read(thread_t *t) { }
 
 void sys_write(thread_t *t) { }
 
-void sys_exit(thread_t *t) { }
+void sys_exit(thread_t *t) 
+{ 
+  delete_from_begin();
+}
 
 void io_complete(thread_t *t) { }
 
@@ -26,4 +53,45 @@ void io_starting(thread_t *t) { }
 stats_t *stats() { 
   int i = 0; // Remove - only here to fail code warnings check
   return NULL; 
+}
+
+
+void insert_at_end(thread_t *td) {
+  struct node *t, *temp;
+
+  t = (struct node*)malloc(sizeof(struct node));
+  t->thread = td;
+  count++;
+
+  if (head == NULL) {
+    head = t;
+    head->next = NULL;
+    return;
+  }
+
+  temp = head;
+
+  while (temp->next != NULL)
+    temp = temp->next;
+
+  temp->next = t;
+  t->next   = NULL;
+}
+
+void delete_from_begin() {
+  struct node *t;
+
+
+  if (head == NULL) {
+    printf("Linked list is empty.\n");
+    return;
+  }
+
+
+  t = head->next;
+  //free(head);
+  head = t;
+  count--;
+
+  printf("%d deleted from the beginning successfully.\n", n);
 }
