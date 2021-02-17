80d90e68a3a12d212a4dbb5c72e29765430c2b27
edarsh7 <edarsh7@gmail.com>
2021-02-17 13:17:59 -0800

    idk

--- scheduler.c
+++ scheduler.c
@@ -80,9 +80,9 @@ void sys_exit(thread_t *t)
   td_running_start(head->thread);
   wait_time(t);
   turnaround(t);
-  printf("tid: %d    ta: %d  wt: %d   \n", td_list->thread->tid, td_list->turnaround, td_list->wait_time);
   td_completed(t);
   delete_from_begin();
+  printf("tid: %d    ta: %d  wt: %d   \n", td_list->thread->tid, td_list->turnaround, td_list->wait_time);
   if(head != NULL)
     sim_dispatch(head->thread);
 }
