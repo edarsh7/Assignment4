b558f662abef82faec41880a91efb70d5417eda5
edarsh7 <edarsh7@gmail.com>
2021-02-17 13:59:18 -0800

    idk

--- scheduler.c
+++ scheduler.c
@@ -122,7 +122,7 @@ stats_t *stats() {
   {
     turnaround(temp->thread);
     wait_time(temp->thread);
-    printf("tid: %d    ta: %d  wt: %d   \n", temp->thread->tid, temp->turnaround, temp->wait_time);
+    printf("tid: %d    ta: %d  wt: %d   \n", temp->thread->tid, temp->turnaround, temp->start2);
     temp = temp->next;
   }
 
