30adacf5abbbaaeab7a8d1ef8d85ca41d5193105
edarsh7 <edarsh7@gmail.com>
2021-02-17 17:51:55 -0800

    idk

--- scheduler.c
+++ scheduler.c
@@ -59,8 +59,11 @@ void sys_exec(thread_t *t)
   insert_td_list(t);
   td_arrival(t);
 
-  left_queue(head->thread);
-  sim_dispatch(head->thread);
+  if(t != head->thread)
+  {
+    left_queue(head->thread);
+    sim_dispatch(head->thread);
+  }
 
 }
 
@@ -69,7 +72,7 @@ void sys_read(thread_t *t)
   delete_from_begin();
 
 
-  if(head != NULL && t != head->thread)
+  if(head != NULL)
   {
     left_queue(head->thread);
     sim_dispatch(head->thread);
