b249bd4689394a19fb197b7b70ab46b84a461bdc
edarsh7 <edarsh7@gmail.com>
2021-02-17 18:40:06 -0800

    idk

--- scheduler.c
+++ scheduler.c
@@ -153,7 +153,7 @@ stats_t *stats() {
   {
     turnaround(temp->thread);
     wait_time(temp->thread);
-    printf("s1: %d    s2: %d  \n", temp->start1, temp->start2);
+    printf("tid: %d  s1: %d    s2: %d  \n",temp->thread->tid, temp->start1, temp->start2);
     temp = temp->next;
   }
 
