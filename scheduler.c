f1cb41e2154d2b4a81cf86c03ca6a68031c30705
edarsh7 <edarsh7@gmail.com>
2021-02-17 19:30:53 -0800

    idk

--- scheduler.c
+++ scheduler.c
@@ -115,6 +115,11 @@ void io_complete(thread_t *t)
   insert_at_end(t);
   io_completed(t);
 
+  if(head->thread == t)
+  {
+    head->start2++;
+  }
+
   if(flag == 0)
   {
     left_queue(head->thread);
