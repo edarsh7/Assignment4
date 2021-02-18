cb6d8efd9ffd638e826da13e0f9b947df5a4e162
edarsh7 <edarsh7@gmail.com>
2021-02-17 20:34:55 -0800

    idk

--- scheduler.c
+++ scheduler.c
@@ -316,7 +316,7 @@ void wait_time(thread_t *td)
   }
   else
   {
-    temp->wait_time = (temp->start1 - temp->arrival) + (temp->start2 - temp->io_done) + (temp->io_start - temp->first_burst_done);
+    temp->wait_time = (temp->start1 - temp->arrival-1) + (temp->start2 - temp->io_done) + (temp->io_start - temp->first_burst_done);
   }
 }
 
